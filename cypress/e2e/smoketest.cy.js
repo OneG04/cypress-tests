ba0b49af91cad03f5041e0667a18566e1f118b37
Juan Gomez <jugogrr@hotmail.com>
2025-05-14 12:57:12 -0500

    añadir el beforeEach

--- cypress/e2e/smoketest.cy.js
+++ cypress/e2e/smoketest.cy.js
@@ -1,14 +1,19 @@
+//const { beforeEach } = require("mocha")
+
 describe("test suit", () =>{
 
-    it("Validar pagina de incio", () =>{
-        //Inicio
-        cy.visit("http://zero.webappsecurity.com/")
-        cy.get('.brand').contains('Zero Bank').should('be.visible')
-        cy.get('#signin_button').click()
-        cy.get('h3').contains('Log in')
-        cy.get('#user_login').type('username')
-        cy.get('#user_password').type('password')
-        cy.get('.btn').click()
+    beforeEach(() => {
+         //Inicio
+         cy.visit("http://zero.webappsecurity.com/")
+         cy.get('.brand').contains('Zero Bank').should('be.visible')
+         cy.get('#signin_button').click()
+         cy.get('h3').contains('Log in')
+         cy.get('#user_login').type('username')
+         cy.get('#user_password').type('password')
+         cy.get('.btn').click()
+    });
+
+    it("Validar acount activity", () =>{
         //acount activity
         cy.get('#account_activity_tab > a').click()
         cy.get('#aa_accountId').select('Checking')
